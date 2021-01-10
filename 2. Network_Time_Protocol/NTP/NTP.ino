c1ce7c7db19735525f45da70b218d7c475332d11
irenecalabuig <71815934+irenecalabuig@users.noreply.github.com>
2021-01-10 16:30:19 +0100

    Ejercicio 2
    Poner en hora el módulo mediante un servidor NTP (Network Time Protocol)

--- 2. Network_Time_Protocol/NTP/NTP.ino
+++ 2. Network_Time_Protocol/NTP/NTP.ino
@@ -1,12 +1,15 @@
+//----------------------------------------------- PRÁCTICA 4 -----------------------------------------------//
+// Ejercicio 2:  Pon en hora el módulo mediante un servidor NTP (Network Time Protocol) 
+
 #include <WiFi.h>
 #include "time.h"
 
-const char* ssid     = "Aise"; // Nombre de mi wifi
-//const char* password = ""; // Contraseña de mi casa
+const char* ssid     = "Aise1";           // Nombre de mi wifi
+const char* password = "01234567";        // Contraseña 
 
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 3600;
-const int   daylightOffset_sec = 3600;
+const char* ntpServer = "pool.ntp.org";   // Servidor horario
+const long  gmtOffset_sec = 3600;         // Compensar UTC para mi zona horaria en ms
+const int   daylightOffset_sec = 3600;    // Compensar la luz del día 
 
 void printLocalTime()
 {
@@ -14,8 +17,9 @@ void printLocalTime()
   if(!getLocalTime(&timeinfo)){
     Serial.println("Failed to obtain time");
     return;
-  }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  }  
+  Serial.println(&timeinfo, " %H:%M:%S");  // para imprimir sólo la hora
+  //Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
 }
 
 void setup()
@@ -32,8 +36,9 @@ void setup()
   Serial.println(" CONNECTED");
   
   //init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer); // inicializar el cliente NTP 
+                                                            // obtener la fecha y la hora de un servidor NTP.
+  printLocalTime();                                         // imprimir hora actual.
 
   //disconnect WiFi as it's no longer needed
   WiFi.disconnect(true);
@@ -43,5 +48,5 @@ void setup()
 void loop()
 {
   delay(1000);
-  printLocalTime();
+  printLocalTime();                                         // imprimir hora actual.
 }
